4d8c1803df0e67690156f30d420b6831b3f2fe7f
AugistinK <g.nadieline@infinitiax.com>
2025-10-28 15:59:48 +0100

    Commit 3

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-// Export all reusable components
-export { default as SkillBar } from './SkillBar';
-export { default as SectionTitle } from './SectionTitle';
-export { default as ExperienceItem } from './ExperienceItem';
-export { default as EducationItem } from './EducationItem';
-export { default as ContactInfo } from './ContactInfo';
-export { default as ProfilePhoto } from './ProfilePhoto';
--- src/templates/index.js
+++
@@ -1,9 +0,0 @@
-// Export all templates
-export { default as ModernProfessional } from './ModernProfessional';
-export { default as TechDeveloper } from './TechDeveloper';
-export { default as CreativeDesigner } from './CreativeDesigner';
-export { default as CorporateExecutive } from './CorporateExecutive';
-export { default as MinimalistClean } from './MinimalistClean';
-
-// Export template configuration
-export { default as templateConfig, getTemplateById, getTemplatesByCategory, getImplementedTemplates, categories } from './templateConfig';
